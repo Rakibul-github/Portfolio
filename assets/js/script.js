34b1f629e5bcb18a82e6255f948466ad7d87011d
Rakibul Hasan <rakibulhasan6733@gmail.com>
2023-01-13 12:01:58 +0600

    Added nav-tabs in protfolio section

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.typing', {
+    strings: [
+        'Rakibul Hasan',
+        'FrontEnd Developer',
+        'Web Designer'
+    ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true,
+    backDelay: 1500,
+    showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+  });
